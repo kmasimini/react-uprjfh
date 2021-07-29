291c06eaac0658fe933dfa3a4eb7c2b62f5b4741
kmasimini <bradley.kmasimini@gmail.com>
2021-07-29 15:00:31 -0700

    1 file modified

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,35 +1,19 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
-import  { useAuth } from "./AuthContext"
+import React from 'react';
 import './todo.css'
 
-
-export default function Dashboard(){
-   const { logout } = useAuth()
-
-   async function handleLogout(){
-     setError("")
-      try{
-         await logout()
-         history.push('/login')
-      }catch{
-        setError('Failed to log out')
-      }
-   }
-
-  return(
-    <>
-    <div className="Dashboard">
+class Dashboard extends React.Component{
+  render(){
+    return(
+      <div className="Dashboard">
        <header>
-        <form id="to-do-form">
-         <input type="text" placeholder="Add task"/>
-         <button type="submit">Add</button>
-         </form>
-        </header>
-      </div>
-     <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>Log Out</Button>
-      </div>
-      </>
-  )
+         <form id="to-do-form">
+           <input type="text" placeholder="Add Task" />
+           <button type="submit">Add</button>
+           </form>
+       </header>
+       </div>
+    );
+  }
+
 }
+export default Dashboard;
