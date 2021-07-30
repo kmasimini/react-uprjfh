59973e0dac35dc973b3f33814d03766b40c37bc5
kmasimini <bradley.kmasimini@gmail.com>
2021-07-29 17:35:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-uprjfh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-uprjfh)
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD4X1rWxULVwnd349dagx7xUIEfU1kppAs",
+  authDomain: "todo-list-7c42f.firebaseapp.com",
+  projectId: "todo-list-7c42f",
+  storageBucket: "todo-list-7c42f.appspot.com",
+  messagingSenderId: "546484060528",
+  appId: "1:546484060528:web:0be06fa3d51b2e8b028fb5"
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
+export default app;
