591e3056c7630fe69e105624d264264956f6663e
kmasimini <bradley.kmasimini@gmail.com>
2021-08-11 15:59:46 +0200

    1 file modified

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
+import {db} from '../firebase';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
+import { ListItemText, Button } from '@material-ui/core';
 
 
 
-function Todo({ input, completeTodo, removeTodo, updatedTodo }){
+function Todo({ input, completeTodo, id, updatedTodo }){
+
+  function deleteTodo(){
+    db.collection("todo").doc(id).delete();
+  }
  
   return(
     <div className="todo-row">
-        <p>{input}</p>
+      <ListItemText primary={input} />
+      <RiCloseCircleLine onClick={deleteTodo} className='delete-icon' />
       </div>
   )       
 }
