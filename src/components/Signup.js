1d9e5e7df54ab94b6b204e6da2c0c000b548b837
kmasimini <bradley.kmasimini@gmail.com>
2021-08-13 11:07:58 +0200

    1 file modified

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -35,7 +35,6 @@ export default function Signup() {
     <>
     <div className="form-container">
      <div className="form-content-left"> 
-        <img src="" alt="" className="form-img" />
         </div>
       <Card>
         <Card.Body>
